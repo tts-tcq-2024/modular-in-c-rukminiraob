d0e04716f5689a4d4db6118f899b8991b327c141
rukminiraob <braorukmini@gmail.com>
2024-09-25 23:04:22 +0530

    new feature

--- main.c
+++ main.c
@@ -8,5 +8,8 @@ int main() {
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
 
+    // Print the formatted color coding manual
+    PrintColorCodingManual();
+
     return 0;
 }
