a5c59b6ab0ab44f2776074eef40df4c4023c379c
rukminiraob <braorukmini@gmail.com>
2024-09-25 22:45:44 +0530

    Create color_pair.h

---
+++ color_pair.h
@@ -0,0 +1,32 @@
+// color_pair.h
+#ifndef COLOR_PAIR_H
+#define COLOR_PAIR_H
+
+#include <stdio.h>
+#include <assert.h>
+
+// Enumerations for major and minor colors
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+// Constant strings for color names
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
+
+extern int numberOfMajorColors;
+extern int numberOfMinorColors;
+
+// Structure to hold a color pair
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
+// Function declarations
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
+
+#endif // COLOR_PAIR_H
