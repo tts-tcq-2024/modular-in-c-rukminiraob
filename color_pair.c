641f42a0566e3575dc852cd3351ff48ef281a912
rukminiraob <braorukmini@gmail.com>
2024-09-26 10:19:27 +0530

    datatype change

--- color_pair.c
+++ color_pair.c
@@ -3,8 +3,8 @@
 // Definitions for major and minor color names
 const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-const int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-const int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 
 // Convert ColorPair to string
