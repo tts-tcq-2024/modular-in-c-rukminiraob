e76cd8edcbef595f98fa23a3df77876db870ba91
rukminiraob <braorukmini@gmail.com>
2024-10-03 10:55:56 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 int main() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
-
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
     
