bcc5651680bc1de3f3827892f6ee88efe11bb881
rukminiraob <braorukmini@gmail.com>
2024-10-03 10:52:31 +0530

    removal of test code

--- color_pair.c
+++ color_pair.c
@@ -5,14 +5,11 @@ const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 const int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 const int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-const int MAX_COLORPAIR_NAME_CHARS = 16;
 
-// Convert ColorPair to string
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
-    sprintf(buffer, "%s %s", MajorColorNames[colorPair->majorColor], MinorColorNames[colorPair->minorColor]);
+    snprintf(buffer, MAX_BUFFER_SIZE, "%s %s", MajorColorNames[colorPair->majorColor], MinorColorNames[colorPair->minorColor]);
 }
 
-// Get ColorPair from pair number
 ColorPair GetColorFromPairNumber(int pairNumber) {
     ColorPair colorPair;
     int index = pairNumber - 1;
@@ -21,24 +18,6 @@ ColorPair GetColorFromPairNumber(int pairNumber) {
     return colorPair;
 }
 
-// Get pair number from ColorPair
 int GetPairNumberFromColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors + colorPair->minorColor + 1;
-}
-
-// Test functions
-void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor) {
-    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
-    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
-    ColorPairToString(&colorPair, colorPairNames);
-    printf("Got pair %s\n", colorPairNames);
-    assert(colorPair.majorColor == expectedMajor);
-    assert(colorPair.minorColor == expectedMinor);
-}
-
-void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber) {
-    ColorPair colorPair = {major, minor};
-    int pairNumber = GetPairNumberFromColor(&colorPair);
-    printf("Got pair number %d\n", pairNumber);
-    assert(pairNumber == expectedPairNumber);
 }
